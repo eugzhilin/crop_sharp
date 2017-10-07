5fc0d241928a49ca4db545dfff9b7100340c25d5
Eprst <Eprst@ST-PC>
2017-10-07 22:39:30 +0300

    TODO

--- Cropper/Cropper/Form1.cs
+++ Cropper/Cropper/Form1.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Cropper
 {
     public partial class Form1 : Form
     {
+        UserRect rect;
         public Form1()
         {
             InitializeComponent();
+
+            rect = new UserRect(new Rectangle(0, 0, 100, 150));
+            rect.SetPictureBox(this.pictureBox1);
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
